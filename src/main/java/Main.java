dea2ea0f688a1dd0e70bd4f60ae789b3928bbc3c
Samuel Sharaf <sam@heroku.com>
2016-11-08 14:27:10 -0800

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello Gap Team!");
+    get("/hello", (req, res) -> "Hello, Gap Team!");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
