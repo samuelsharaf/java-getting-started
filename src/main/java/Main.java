50ea04a298824c3b0bbe4d314e7c57f6526d8510
Samuel Sharaf <sam@heroku.com>
2016-11-14 13:11:14 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello, AWS Team!");
+    get("/hello", (req, res) -> "Hello, Intel Team!");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello Gap!");
+            attributes.put("message", "Hello Intel!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
